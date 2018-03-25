e4ade58e1399e8623a1e34ec9a2dfda4bffe17f6
Max Vagner <sydneymax@gmail.com>
2018-03-25 22:36:15 +1100

    Create Products controller

---
+++ WebApiDemo/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
